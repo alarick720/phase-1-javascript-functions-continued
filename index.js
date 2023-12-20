e1df3d4e3a2ae4fab7bc08dddde6a6e7ff7284c3
Alarick Whitted <awhitted834@gmail.com>
2023-12-20 08:56:46 -0500

    Develop
    git
    
    git commit-m Done

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// code your solution here
+function saturdayFun(activity= "roller-skate"){
+    console.log ("function exists");
+            return `This Saturday, I want to ${activity}!`
+}
+      
+     function mondayWork(activity= "go to the office"){ 
+            console.log ("function exist");
+            return `This Monday, I will ${activity}.`
+     }
+      
+     function wrapAdjective(highlight ="*special") {
+        return function(adjective) {
+          return `You are ${highlight}${adjective}${highlight}!`;
+        };
+          
+        }
+    
+
+        
+        
+
+      
+
+    
+
+
+            
+        
+            
+           
+            
+        
+
+        
+        
+        
+    
+   
+
